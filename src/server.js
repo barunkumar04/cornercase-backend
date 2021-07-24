27872e3f156e614557ad1265c0751f5288e7608c
Barun Kumar <barun.kumar04@gmail.com>
2021-07-24 23:15:55 +0530

    Collating frontend and backend projects

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/build'))); // To support frontend calls: use static file
 app.use(bodyParser.json());
 
 const withDB = async (operations, res) => {
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('cornercase');
-
+    
         await operations(db);
-
+    
         client.close();
     } catch (error) {
         res.status(500).json({ message: 'Error connecting to db', error });
@@ -31,7 +33,7 @@ app.get('/api/articles/:name', async (req, res) => {
 app.post('/api/articles/:name/upvote', async (req, res) => {
     withDB(async (db) => {
         const articleName = req.params.name;
-
+    
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
@@ -39,7 +41,7 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
             },
         });
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
-
+    
         res.status(200).json(updatedArticleInfo);
     }, res);
 });
@@ -59,6 +61,11 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 
         res.status(200).json(updatedArticleInfo);
     }, res);
+});
+
+// To support frontend calls: to allow navigation
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
